e79f112cc27de6e04503b22dec304f539a92a7b5
Deuskino <alexandresilvacarvalho23@gmail.com>
2025-11-16 11:04:00 -0300

    Mestre
    Updated data types for population variables to unsigned long int and adjusted scanf format specifiers accordingly. Enhanced output formatting for population and super power calculations.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,16 +5,21 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
     //Carta1
-    int turistico, pop;
+    int turistico;
+    unsigned long int pop;
     char estado, codigo[5], nome[20];
     double pib, area;
     float densidade, pibpercapita;
+    float sp, inversodp;
 
     //Carta2
-    int turistico2, pop2;
+    int turistico2;
+    unsigned long int pop2;
     char estado2, codigo2[5], nome2[20];
     double pib2, area2;
     float densidade2, pibpercapita2;
+    float sp2, inversodp2;
+
 
   // Área para entrada de dados
     printf("!!Primeira Carta!! \n");
@@ -29,7 +34,7 @@ int main() {
     scanf(" %19[^\n]%*c", nome); 
 
     printf("Digite a população da cidade: \n");
-    scanf(" %d", &pop);
+    scanf(" %u", &pop);
 
     printf("Informe a área da cidade em km²: \n");
     scanf(" %lf", &area);
@@ -59,7 +64,7 @@ int main() {
     scanf(" %19[^\n]%*c", nome2); 
 
     printf("Digite a população da cidade: \n");
-    scanf(" %d", &pop2);
+    scanf(" %u", &pop2);
 
     printf("Informe a área da cidade em km²: \n");
     scanf(" %lf", &area2);
@@ -78,16 +83,55 @@ int main() {
   // Área para exibição dos dados da cidade
         //primeira carta
     printf("\n >>> Carta 1 <<< \n");
-    printf("Estado %c\nCódigo: %c%s\nCidade: %s\nPopulação: %d habitantes\nÁrea: %.2lf km²\nPIB: %.2lf bilhões de reais\nPontos turísticos: %d\n", estado, estado, codigo, nome, pop, area, pib, turistico);
+    printf("Estado %c\nCódigo: %c%s\nCidade: %s\nPopulação: %u habitantes\nÁrea: %.2lf km²\nPIB: %.2lf bilhões de reais\nPontos turísticos: %d\n", estado, estado, codigo, nome, pop, area, pib, turistico);
     printf("Densidade populacional: %.2f\ hab/km²\n", densidade);
     printf("PIB per capita: %.2f reais\n", pibpercapita);
+      inversodp = 1 / densidade;
+      sp = (float) (pop + area + pib + turistico + pibpercapita + inversodp);
+    printf("Super Poder: %.2f\n", sp);
 
         //segunda carta
     printf("\n >>> Carta 2 <<< \n");
-    printf("Estado: %c\nCódigo: %c%s\nCidade: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nPontos turísticos: %d\n", estado2, estado2, codigo2, nome2, pop2, area2, pib2, turistico2);
+    printf("Estado: %c\nCódigo: %c%s\nCidade: %s\nPopulação: %u habitantes\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nPontos turísticos: %d\n", estado2, estado2, codigo2, nome2, pop2, area2, pib2, turistico2);
     printf("Densidade populacional: %.2f hab/km²\n", densidade2);
     printf("PIB per capita: %.2f reais\n", pibpercapita2);
+      inversodp2 = 1 / densidade2;
+      sp2 = (float) (pop2 + area2 + pib2 + turistico2 + pibpercapita2 + inversodp2);
+    printf("Super Poder: %.2f\n", sp2);
 
   
+  // Fim da exibição dos dados da cidade
+  
+ 
+  // Área para comparação das cartas
+  printf("\n >>> Resultado da comparação <<< \n");
+    printf("População: Carta %d venceu (%d)\n",
+       (pop > pop2 ? 1 : 2),
+       (pop > pop2));
+
+    printf("Área: Carta %d venceu (%d)\n",
+       (area > area2 ? 1 : 2),
+       (area > area2));
+
+    printf("PIB: Carta %d venceu (%d)\n",
+       (pib > pib2 ? 1 : 2),
+       (pib > pib2));
+
+    printf("Pontos turísticos: Carta %d venceu (%d)\n",
+       (turistico > turistico2 ? 1 : 2),
+       (turistico > turistico2));
+
+    printf("Densidade populacional: Carta %d venceu (%d)\n",
+      (densidade < densidade2 ? 1 : 2),
+      (densidade < densidade2 ));
+
+    printf("PIB per capita: Carta %d venceu (%d)\n",
+      (pibpercapita > pibpercapita2 ? 1 : 2),
+      (pibpercapita > pibpercapita2 ));
+
+    printf("Super Poder: Carta %d venceu (%d)\n",
+      (sp > sp2 ? 1 : 2),
+      (sp > sp2 ));
+
 return 0;
 } 
